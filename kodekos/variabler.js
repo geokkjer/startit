bd86d3a373c8527b43fbe3aba641b9be90189658
Geir Okkenhaug Jerstad <geir@geokkjer.eu>
2024-08-21 11:02:05 +0200

    this and functions

---
+++ kodekos/variabler.js
@@ -0,0 +1,13 @@
+
+// Terje er født med ski på bena.
+// Marie har en bror.
+// Broren til Marie heter Bjarne.
+// Terje er 45 år gammel.
+// Terje har på seg Hawaii skjorte.
+
+let name 
+let bornWithOnLegs
+let hasBrother
+let brotherName
+let age
+let wearing
