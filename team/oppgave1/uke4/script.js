48069b01c72aac7d2ac3a1b2c706abcefdfdc697
Geir Okkenhaug Jerstad <geir@geokkjer.eu>
2024-08-28 14:12:05 +0200

    some work on team assignment

--- team/oppgave1/uke4/script.js
+++ team/oppgave1/uke4/script.js
@@ -1,5 +1,4 @@
 // Model
-let lifeTimer = 100;
 let foodTime = 10;
 let petTime = 10;
 let foodElement = document.getElementById("foodId");
@@ -7,31 +6,36 @@ let petElement = document.getElementById('petId');
 let frogImage = document.getElementById('tamaFrog').src = "img/default.jpg";
 
 
-setInterval(foodFunction, 1000)
+setInterval(foodFunction, 1000);
+setInterval(petFunction, 1000);
 
 // View
+// function reset() {
+//     if (foodTime < 0){
+//         foodTime += 50;
+//         petTime += 10; 
+//         defaultImg();   
+//     }
+// }
 function foodFunction() {
-
     if (foodTime < 0) {
         //game over???
         document.getElementById('tamaFrog').src = "img/dead.jpg";
-        document.getElementById('tamaFrog').style.backgroundColor = 'red';
-        setInterval(afterLife, 1000);
+        // setTimeout(afterLife, 1000);
     } else {
         foodElement.innerHTML = foodTime + " seconds remaining"
         foodTime--
-        if (foodTime < 5) {
+        if (foodTime < 15) {
             document.getElementById('tamaFrog').src = "img/need_food.jpg";
         }
     }
 }
 // Controller
 function feed() {
-    if (foodTime > 0) {
+    if (foodTime > 0 && petTime > 5) {
         foodTime += 10;
         document.getElementById('tamaFrog').src = "img/eating.jpg";
         setTimeout(defaultImg, 500)
-
     }
 
 }
@@ -41,24 +45,24 @@ function defaultImg() {
 }
 
 function petFunction() {
-    if (petTime < 0) {
-
-        document.getElementById('petFrog').src = "img/need_petting.jpg";
-    } else {
-        petElement.innerHTML = petTime + "seconds remaining"
-        foodTime--
-        if (petTime < 3) {
-            document.getElementById('petFrog').src = "img/need_love.jpg";
-        }
+    if (petTime < 5) {
+        document.getElementById('tamaFrog').src = "img/angry.jpg";
+    }
+    else {
+        petElement.innerHTML = petTime + " seconds remaining"
+        petTime--
+       
     }
 }
 function pet() {
-    if (petTime > 0) {
+    if (foodTime > 0) {
         petTime += 10;
-        document.getElementById('petFrog').src = "img/need_love.jpg";
+        document.getElementById('tamaFrog').src = "img/need_love.jpg";
         setTimeout(defaultImg, 500)
-
     }
+        
+
+    
 
 }
 function defaultImg() {
