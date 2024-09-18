166b85b5e7e3709aaf84e857ab3283b0c78cc902
Geir Okkenhaug Jerstad <geir@geokkjer.eu>
2024-09-18 12:20:01 +0200

    reaksjonstid not working yet

--- oppgaver/uke7/reaksjonstid/mvc.js
+++ oppgaver/uke7/reaksjonstid/mvc.js
@@ -1,15 +1,23 @@
+
+
 // model
 var app = document.getElementById('app');
 var html = ' ';
 var lampArray = [];
+var selectedLampIndex = 0;
 var lightOn;
+var clickLamp = document.getElementById('selectedLampIndex')
+var startTime = new Date().getTime();
+var finishTime = new Date().getTime();
+var spentMilliseconds = Math.floor(finishTime - startTime);
+var spentSeconds = spentMilliseconds / 1000;
 
 // view
 updateView();
 twentyFive();
 lightUpRandomLamp();
 function updateView() {
-    app.innerHTML = html + `<button onclick="lightUpRandomLamp()">Light up random lamp</button>`;
+    app.innerHTML = html + /*HTML*/` <h2>Klikk på en lampe for å skifte</h2><div id="showTime">timeSpent</div>`;
 }
 
 // controller
@@ -30,11 +38,17 @@ function twentyFive() {
 function lightUpRandomLamp() {
     checkForLightOn();
     let divToReplace = ' ';
-    let selectedLampIndex = Math.floor(Math.random() * lampArray.length);
+    selectedLampIndex = Math.floor(Math.random() * lampArray.length);
     divToReplace = document.getElementById(selectedLampIndex);
     let divToreplaceWith = document.getElementById(selectedLampIndex).classList = `circles  lightOn`
-    html = html.replace(divToReplace, divToreplaceWith);
+    html = html.replace(divToReplace, divToreplaceWith)
+    setOnClick = document.getElementById(selectedLampIndex).onclick = lightUpRandomLamp;
+    startTimer();
+    html = html + setOnClick
+
     clearScreen();
+    
+
 }
 function clearScreen() {
     html = '';
@@ -42,8 +56,26 @@ function clearScreen() {
 function checkForLightOn() {
     for (i = 0; i < lampArray.length; i++) {
         document.getElementById(i).classList.contains('circles')
-            if (document.getElementById(i).classList.contains("lightOn")) {
-                document.getElementById(i).classList = `circles`;
-            }
+        if (document.getElementById(i).classList.contains("lightOn")) {
+            document.getElementById(i).classList = `circles`;
         }
     }
+}
+function startTimer() {
+    clickLamp.addEventListener("click", stopTimer)
+    var currentTime = new Date().getTime();
+    let timeSpent = Math.floor(currentTime - startTime)
+    let timeSpentSeconds = (timeSpent / 1000)
+
+    console.log(timeSpentSeconds)
+}
+
+function stopTimer() {
+    document.getElementById(selectedLampIndex).removeEventListener("click", stopTimer)
+
+    var currentTime = new Date().getTime();
+    let timeSpent2 = Math.floor(currentTime - startTime)
+    newTime = (timeSpent2 / 1000)
+    console.log(newTime);
+    document.getElementById(selectedLampIndex).addEventListener("click", startTimer)
+}
