98723969eb6cf4152b1e13a949e97cad319b9de2
Geir Okkenhaug Jerstad <geir@geokkjer.eu>
2024-09-23 12:41:34 +0200

    jobbet litt med arrow functions

---
+++ eloquentjavascript/05_higher_order_functions/transforming_with_map.js
@@ -0,0 +1,11 @@
+const SCRIPTS = require("./scripts.js");
+
+function map(array, transform) {
+    let mapped = [];
+    for (let element of array){
+        mapped.push(transform(element));
+    }
+    return mapped
+}
+let rtlScripts = SCRIPTS.filter(s => s.direction == 'rtl');
+console.log(map(rtlScripts, s => s.name));
