3648937eac8fee941b87242ef0a95143ee2b942d
Geir Okkenhaug Jerstad <geir@geokkjer.eu>
2024-09-25 14:42:53 +0200

    test spa with react

---
+++ react_test/spa_react/src/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h3>SPA App - Home</h3>
+                <p>This is a paragraph on the HomePAge of the SPA App.</p>
+            </div>
+        );
+    }
+}
+export default Home;
