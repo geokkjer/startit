72272476ff9733aa49c96342062b646ad6f981e1
Geir Okkenhaug Jerstad <geir@geokkjer.eu>
2024-10-04 10:29:31 +0200

    vinlotteri oppgave

--- eloquentjavascript/06_Objects/obj.js
+++ eloquentjavascript/06_Objects/obj.js
@@ -13,3 +13,22 @@ whiteRabbit.speak("Oh my fur and whiskers");
 
 hungryRabbit.speak("Got any carrots?");
 
+let finder = {
+    find(array){
+        return array.some(v => v == this.value)
+    },
+    value: 5
+};
+console.log(finder.find([4, 5]))
+const array = [1, 2, 3, 4, 5];
+
+// Checks whether an element is even
+const even = (element) => element % 2 === 0;
+
+console.log(array.some(even));
+
+let empty = {
+    name: "Geir",
+    age: 51,
+};
+console.log(empty.age.toLocaleString(), ++empty.age);
