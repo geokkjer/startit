95d3a2be13477a89ed2058d38d0d22221cd7ee5d
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2024-11-14 14:08:38 +0100

    RND

--- Emne 3/ConsoleApp4/IfElse/Program.cs
+++ Emne 3/ConsoleApp4/IfElse/Program.cs
@@ -7,6 +7,13 @@
         private static bool IsEqual = false;
         static void Main(string[] args)
         {
+           Compare();
+           UnEqual(5, 5);
+           IsThirty(25, 5);
+        }
+
+        static void Compare()
+        {
             if (num == num2)
             {
                 Console.WriteLine("Equal");
@@ -17,7 +24,7 @@
                 Console.WriteLine("Not Equal");
                 IsEqual = false;
             }
-
+            
             if (IsEqual)
             {
                 num++;
@@ -27,6 +34,30 @@
             {
                 num--;
                 Console.WriteLine(num);
+            }
+        }
+
+        static int UnEqual(int num1, int num2)
+        {
+            if (num1 != num2)
+            {
+               return num1 + num2; 
+            }
+            else
+            {
+                return num1 * num2;
+            } 
+        }
+
+        static bool IsThirty(int num, int num2)
+        {
+            if (num + num2 == 30)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
     }
---
+++ Emne 3/ConsoleApp5/Random/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Random
+{
+    class Random
+    {
+        static void Main(string[] args)
+        {
+            System.Random rnd = new System.Random();
+            int randomnr = rnd.Next(0, 3);
+            if (randomnr == 0)
+            {
+                Console.WriteLine("Tallet ble 0");
+            }else if (randomnr == 1)
+            {
+                Console.WriteLine("Tallet ble 1");
+            }
+            else
+            {
+                Console.WriteLine("Tallet ble 2");
+            }
+        }
+    }
+    
+}
