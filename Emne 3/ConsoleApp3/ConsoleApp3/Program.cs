eee5472033a26dae4138b7c6efd2b41dc80751b4
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2024-11-14 11:31:51 +0100

    testing D♭

--- Emne 3/ConsoleApp3/ConsoleApp3/Program.cs
+++ Emne 3/ConsoleApp3/ConsoleApp3/Program.cs
@@ -3,11 +3,27 @@
     internal class Program
     {
         static void Main(string[] args)
-        { 
-            int a = 3;
-            decimal b = 5;
-            decimal sum = a + b;
-            Console.WriteLine(sum);
+        {
+            Run();
+        }
+
+        public static decimal sum()
+        {
+             int a = 3;
+             decimal b = 5;
+             decimal sum = a + b;
+             return sum;
+
+        }
+        private static decimal Sum(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public static void Run()
+        {
+            Console.WriteLine(sum());
+            Console.WriteLine(Sum(3,5));
         }
     }
 }
