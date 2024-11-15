2c097039f6ef72eed0c93ede252169d3579aefe1
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2024-11-15 12:17:27 +0100

    SwitchOppgave

---
+++ Emne 3/SwitchOppgaver/SwitchOppgaver/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SwitchOppgaver
+{
+    class SwitchDay
+    {  
+        static private void WhichDay(){
+        Console.WriteLine("Enter Day Number: ");
+        var day= Convert.ToInt32(Console.ReadLine());
+
+        var WhichDay = day switch
+        {
+            1 => "Monday",
+            2 => "Tuesday",
+            3 => "Wednesday",
+            4 => "Thursday",
+            5 => "Friday",
+            6 => "Saturday",
+            7 => "Sunday",
+            _ => "Invalid day",
+        };
+        Console.WriteLine($"{WhichDay}");
+        }
+
+        class Run
+        {
+            static void Main()
+            {
+                WhichDay();
+            }
+        }
+    }   
+}
