3118086163dac3667a76a9023c25b69a27ad71e1
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2024-11-20 10:31:21 +0100

    Gjett tallet

---
+++ Emne 3/GjettTallet/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GjettTallet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            int tilfeldigTall = rnd.Next(1, 100);
+            bool running = true;
+            var størreEllerMindre = "Du har ikke valgt et tall ennå";
+            while (running == true)
+            {
+                Console.WriteLine($"{størreEllerMindre}");
+                Console.WriteLine("Gjett ett tall mellom 1 - 100: ");
+                var gjettetTallString = Console.ReadLine();
+                int gjettetTall;
+                int.TryParse(gjettetTallString, out gjettetTall);
+                if (gjettetTall == 0)
+                {
+                    størreEllerMindre = "Enten 0 eller ikke ett tall";
+                    Console.Clear();
+                }
+                else if (tilfeldigTall == gjettetTall)
+                {
+                    Console.WriteLine("Du har gjettet riktig tall");
+                    running = false;
+                }
+                else if (tilfeldigTall > gjettetTall)
+                {
+                    størreEllerMindre = $"Det riktige tallet er høyere, ditt siste tall var: {gjettetTall}";
+                    Console.Clear();
+                }
+                else if (tilfeldigTall < gjettetTall)
+                {
+                    størreEllerMindre = $"Det riktige tallet er lavere, ditt siste tall var: {gjettetTall}";
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
