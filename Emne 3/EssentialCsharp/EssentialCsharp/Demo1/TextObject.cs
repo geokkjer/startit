384efef3b2d1b687f674fff15e0ba23880d1f91a
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2024-11-21 14:40:36 +0100

    test

---
+++ Emne 3/EssentialCsharp/EssentialCsharp/Demo1/TextObject.cs
@@ -0,0 +1,8 @@
+namespace EssentialCsharp.Demo1;
+
+public class TextObject
+{
+    public string Text { get; set; }
+    public int FontSize { get; set; }
+    public int FontColor { get; set; }
+}
--- Emne 3/EssentialCsharp/EssentialCsharp/Program.cs
+++ Emne 3/EssentialCsharp/EssentialCsharp/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello World!");
+﻿namespace EssentialCsharp
+{
+    public class Tekst
+    {
+        public static void Main()
+        {
+            var tekst = new TextObject[]
+            {
+                new TextObject
+                {
+                    Text = "Geir",
+                    FontSize = 14,
+                    FontColor = 00001,
+                    Color = ConsoleColor.Green,
+                },
+                new TextObject
+                {
+                    Text = "Geir2",
+                    FontSize = 12,
+                    FontColor = 010001,
+                    Color = ConsoleColor.Gray,
+                }
+            };
+            foreach (var tekst1 in tekst)
+            {
+                tekst1.Print();
+                Thread.Sleep(100);
+            }
+
+        }
+    }
+}
+
