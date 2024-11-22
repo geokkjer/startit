3a981d068f40c9e620b7fce5d2cc1f977c0df357
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2024-11-22 12:21:29 +0100

    Pokemon start

--- Emne 3/Pokemon/Program.cs
+++ Emne 3/Pokemon/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿namespace Pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pikachu = new Pikachu();
+            Console.WriteLine($"Pikachu har {Pikachu.Health}hp og er level {Pikachu.Level}");
+        }
+    }
+}
+
---
+++ Emne 3/Pokemon/pokemon.cs
@@ -0,0 +1,7 @@
+namespace Pokemon;
+
+ public class Pikachu
+{
+   internal static int Health = 50;
+   internal static int Level = 21;
+}
