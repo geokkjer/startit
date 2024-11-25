03716695f9aa9174963318882ba1612552cea09b
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2024-11-25 10:29:48 +0100

    Book class

---
+++ Emne 3/Books/Books/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Books
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Title: {Title}, Author: {Author}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.Author = "John Doe";
+            book.Title= "Hello World!";
+            book.ShowInfo();
+        }
+    }
+}
