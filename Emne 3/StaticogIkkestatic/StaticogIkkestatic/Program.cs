5273e3e1419c9e80b538994c08b51d1211dfdaa8
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2024-11-29 14:38:05 +0100

    Static

---
+++ Emne 3/StaticogIkkestatic/StaticogIkkestatic/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace StaticogIkkestatic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            ShowSeperatorRow();
+            Console.WriteLine("Navn: Stavern");
+            Console.WriteLine("Kommune: Larvik");
+            Console.WriteLine("Fylke: Vestfold");
+            ShowSeperatorRow();
+        }
+        static void ShowSeperatorRow()
+        {
+            Console.WriteLine("****************************");
+        }
+    }
+}
+
