0950feff85ffd1d2544a8eddb10a1552c7ff5cfc
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2024-12-02 08:08:22 +0100

    Monday

--- Emne 3/StaticogIkkestatic/StaticogIkkestatic/Program.cs
+++ Emne 3/StaticogIkkestatic/StaticogIkkestatic/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         { 
             ShowSeperatorRow();
+            var label = "Navn";
+            var fieldValue = "Stavern";
             Console.WriteLine("Navn: Stavern");
             Console.WriteLine("Kommune: Larvik");
             Console.WriteLine("Fylke: Vestfold");
