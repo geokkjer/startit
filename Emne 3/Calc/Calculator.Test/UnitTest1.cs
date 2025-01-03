19b899b5575816117815e5dd9e359c4dcf294933
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-01-03 13:34:56 +0100

    addded test for negative numbers

--- Emne 3/Calc/Calculator.Test/UnitTest1.cs
+++ Emne 3/Calc/Calculator.Test/UnitTest1.cs
@@ -8,6 +8,14 @@ public class Tests
         var calc = new Calc.Calculator();
         var sum = calc.LeggSammen(1, 2);
         Assert.That(sum, Is.EqualTo(3));
+        
+    }
+    [Test]
+    public void TestLeggSammenNetgativ() 
+    { 
+        var calc = new Calc.Calculator();
+        var sum = calc.LeggSammen(-1, 2); 
+        Assert.That(sum, Is.EqualTo(1));
     }
 
     [Test]
@@ -17,13 +25,26 @@ public class Tests
         var sum = calc.TrekkFra(2, 1);
         Assert.That(sum, Is.EqualTo(1));
     }
-
+    [Test]
+    public void TestTrekkFraNetgativ()
+    {
+        var calc = new Calc.Calculator();
+        var sum = calc.TrekkFra(1, -2);
+        Assert.That(sum, Is.EqualTo(3));
+    }
     [Test]
     public void TestMultipliser()
     {
         var calc = new Calc.Calculator();
         var sum = calc.Multipliser(2, 2);
         Assert.That(sum, Is.EqualTo(4));
+    }
+    [Test]
+    public void TestMultipliserNegativ()
+    {
+        var calc = new Calc.Calculator();
+        var sum = calc.Multipliser(3, -3);
+        Assert.That(sum, Is.EqualTo(-9));
     }
 
     [Test]
@@ -33,7 +54,13 @@ public class Tests
         var sum = calc.Divider(4, 2);
         Assert.That(sum, Is.EqualTo(2));
     }
-
+    [Test]
+    public void TestDivisionNegative()
+    {
+        var calc = new Calc.Calculator();
+        var sum = calc.Divider(4, -2);
+        Assert.That(sum, Is.EqualTo(-2));
+    }
     [Test]
     public void TestDivisionByZero()
     {
